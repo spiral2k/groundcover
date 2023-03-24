db5cb16370a93a2dfa34d28a1db1488888d91401
Meni Edri <meniedri1@gmail.com>
2023-03-24 09:43:23 +0300

    relocate config

--- src/common/components/chart/Chart.tsx
+++ src/common/components/chart/Chart.tsx
@@ -2,8 +2,7 @@ import { ApexOptions } from "apexcharts";
 import ApexChart from "react-apexcharts";
 import Img from "src/common/components/Img";
 import CPUIcon from "src/assets/cpu.png";
-import format from "date-fns/format";
-
+import defaultOptions from './defaultOptions';
 import css from "./Chart.module.scss";
 
 const Chart = ({
@@ -35,58 +34,3 @@ const Chart = ({
 };
 
 export default Chart;
-
-const defaultOptions: ApexOptions = {
-  chart: {
-    toolbar: {
-      show: false,
-    },
-    zoom: {
-      enabled: false,
-    },
-  },
-  legend: {
-    position: "right",
-    markers: {
-      radius: 0,
-    },
-  },
-  fill: {
-    type: "gradient",
-    gradient: {
-      opacityFrom: 0.2,
-      opacityTo: 0,
-    },
-  },
-  colors: ["#FF5DA0", "#0066FF", "#FFDE54"],
-  grid: {
-    show: false,
-  },
-  stroke: {
-    width: 2,
-    curve: "straight",
-    dashArray: [0, 8, 0],
-  },
-  yaxis: {
-    tickAmount: 1,
-    min: 0,
-    labels: {
-      formatter: function (val: number) {
-        return val + "";
-      },
-    },
-  },
-  xaxis: {
-    tickAmount: 4,
-    labels: {
-      formatter: function (value, timestamp) {
-        if (!timestamp || timestamp < 0) return "";
-        return format(new Date(timestamp), "HH:mm");
-      },
-    },
-  },
-  tooltip: {
-    shared: false,
-    intersect: true,
-  },
-};
---
+++ src/common/components/chart/defaultOptions.ts
@@ -0,0 +1,60 @@
+import { ApexOptions } from "apexcharts";
+import format from "date-fns/format";
+
+const defaultOptions: ApexOptions = {
+    chart: {
+      toolbar: {
+        show: false,
+      },
+      zoom: {
+        enabled: false,
+      },
+    },
+    legend: {
+      position: "right",
+      markers: {
+        radius: 0,
+      },
+    },
+    fill: {
+      type: "gradient",
+      gradient: {
+        opacityFrom: 0.2,
+        opacityTo: 0,
+      },
+    },
+    colors: ["#FF5DA0", "#0066FF", "#FFDE54"],
+    grid: {
+      show: false,
+    },
+    stroke: {
+      width: 2,
+      curve: "straight",
+      dashArray: [0, 8, 0],
+    },
+    yaxis: {
+      tickAmount: 1,
+      min: 0,
+      labels: {
+        formatter: function (val: number) {
+          return val + "";
+        },
+      },
+    },
+    xaxis: {
+      tickAmount: 4,
+      labels: {
+        formatter: function (value, timestamp) {
+          if (!timestamp || timestamp < 0) return "";
+          return format(new Date(timestamp), "HH:mm");
+        },
+      },
+    },
+    tooltip: {
+      shared: false,
+      intersect: true,
+    },
+  };
+  
+
+  export default defaultOptions
